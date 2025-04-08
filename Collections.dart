f68de16dea9e56b1d5baab9b8ab24ea463f8ed8c
Ranjithkumar7200 <ranjithkumarb8072@gmail.com>
2025-04-08 20:34:38 +0530

    collections learning

---
+++ Collections.dart
@@ -0,0 +1,26 @@
+void main() {
+  //Collections
+  //1)Lists
+  //A List is like a shopping list where order matters
+//Examples:
+
+  // List<String> fruits = ["Apple", "Orange", "Banana"];
+  // for (var result in fruits) {
+  //   print(result);
+  // }
+
+  //2)Sets
+  //A Sets is like a collection of unique  students Ids
+//Examples:
+  // Set<int> uniqueNumbers = {1, 2, 3, 4, 5, 6};
+  // print(uniqueNumbers);
+
+  //3)Maps
+  // A Maps like a dictionary where each word(key) has a meaning(value)
+  // Map<dynamic, dynamic> studentScores = {"John": "gdgdgf", 90: 80, "Bob": 70};
+  // studentScores.forEach(
+  //   (key, value) {
+  //     print("$key : $value");
+  //   },
+  // );
+}
